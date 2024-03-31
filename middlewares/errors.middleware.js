e5cd3630df960d5a6be1332c2cdf7e48d4b5ca18
theomaro <theo.pombe@gmail.com>
2024-03-31 12:47:16 +0300

    FIX: validation error

--- middlewares/errors.middleware.js
+++ middlewares/errors.middleware.js
@@ -7,14 +7,16 @@ const errorHandler = (error, req, res, next) => {
 };
 
 const validationErrorHandler = (error, req, res, next) => {
-  let message = error.message;
-  message = message.includes("[ref:") ? "Password does not match" : message;
+  if (error.name === "ValidationError") {
+    let message = error.details[0].message;
+    message = message.includes("[ref:") ? "Password does not match" : message;
 
-  return res.status(409).json({
-    success: false,
-    type: "validationError",
-    message,
-  });
+    return res.status(409).json({
+      success: false,
+      type: error.name,
+      message,
+    });
+  } else next(error);
 };
 
 export { errorHandler, validationErrorHandler };
