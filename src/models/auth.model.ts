21c78fb90442bdfa46f5de517f42bebf272f5707
theomaro <theo.pombe@gmail.com>
2024-04-03 21:03:10 +0300

    enable register action

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -20,15 +20,13 @@ class UserAuth {
   public isEmailExist = async (email: string) => {
     const sql = "SELECT email FROM user WHERE email=?;";
     const [rows, _] = await db.query<RowDataPacket[]>(sql, [email]);
-    const rowData = rows[0];
-    return rowData ? true : false;
+    return rows[0] ? true : false;
   };
 
   public isUsernameExist = async (username: string) => {
     const sql = "SELECT username FROM user WHERE username=?;";
     const [rows, _] = await db.query<RowDataPacket[]>(sql, [username]);
-    const rowData = rows[0];
-    return rowData ? true : false;
+    return rows[0] ? true : false;
   };
 
   public getUserBy = async (username: string) => {
